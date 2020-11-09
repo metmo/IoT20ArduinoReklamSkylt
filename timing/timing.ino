667ac630e63d2294256a093baf3d852b6146c35e
Vincent <35411050+avipami@users.noreply.github.com>
2020-11-09 10:20:13 +0100

    9nov
    Upload

--- timing/timing.ino
+++ timing/timing.ino
@@ -1,3 +1,15 @@
+#include <LiquidCrystal.h>
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_GrayOLED.h>
+#include <Adafruit_SPITFT.h>
+#include <Adafruit_SPITFT_Macros.h>
+#include <gfxfont.h>
+
+#include <Adafruit_SSD1306.h>
+#include <splash.h>
+
+#include <stdlib.h> 
 void setup() {
   // put your setup code here, to run once:
 
