733205d0ee4717abd4e38b849139a10bb875ff28
metmo <69970419+metmo@users.noreply.github.com>
2020-11-09 16:42:05 +0100

    Added include for customer.h

--- main/main.ino
+++ main/main.ino
@@ -1,25 +1,32 @@
-<<<<<<< Updated upstream
-=======
-#include <LiquidCrystal.h>
 
+#include <LiquidCrystal.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_GrayOLED.h>
 #include <Adafruit_SPITFT.h>
 #include <Adafruit_SPITFT_Macros.h>
 #include <gfxfont.h>
-
 #include <Adafruit_SSD1306.h>
 #include <splash.h>
-
 #include <stdlib.h>
 
->>>>>>> Stashed changes
+#include "customer.h"
+
+
+customerStruct customers[NUMBER_OF_CUSTOMERS];
+
+
 void setup() {
-  // put your setup code here, to run once:
+  populateCustomerStruct(customers, NUMBER_OF_CUSTOMERS);
+
+  Serial.begin(9600);
+  delay(1);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  Serial.println(customers[0].messages[0].text);
+  Serial.println(customers[0].messages[1].text);
+  Serial.println(customers[0].messages[2].text);
 
 }
